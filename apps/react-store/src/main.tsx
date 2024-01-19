11e9ff5307b0f6c80c4721e03c63bb438a30938d
Ferney Moreno <ferney.moreno@pluto.tv>
2024-01-19 14:27:52 -0500

    setting up project

--- apps/react-store/src/main.tsx
+++ apps/react-store/src/main.tsx
@@ -3,11 +3,31 @@ import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
 
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+import {
+  STORE_FEATURE_KEY,
+  storeReducer,
+} from './apps/react-store/store.slice';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const store = configureStore({
+  reducer: { [STORE_FEATURE_KEY]: storeReducer },
+  // Additional middleware can be passed to this array
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: process.env.NODE_ENV !== 'production',
+  // Optional Redux store enhancers
+  enhancers: [],
+});
+
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>
 );
